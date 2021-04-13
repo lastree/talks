1f9d8f7859e7559f8ce1e1f4024f092f49b365c5
Julia <lastree@users.noreply.github.com>
2021-04-14 00:01:38 +0200

    project dependencies

---
+++ install.R
@@ -0,0 +1,8 @@
+install.packages("caret")
+install.packages("Ckmeans.1d.dp")
+install.packages("DiagrammeR")
+install.packages("precrec")
+install.packages("SHAPforxgboost")
+install.packages("tidyverse")
+install.packages("xgboost")
+
