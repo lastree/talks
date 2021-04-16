0519e14ee8cd70d1b028dbcb733999c2d2373e68
Julia <lastree@users.noreply.github.com>
2021-04-16 18:51:20 +0200

    Update install.R

--- install.R
+++ install.R
@@ -1,8 +1,12 @@
 install.packages("caret")
 install.packages("Ckmeans.1d.dp")
 install.packages("DiagrammeR")
+install.packages("e1071")
 install.packages("precrec")
+devtools::install_github("thomasp85/ggforce")
 install.packages("SHAPforxgboost")
 install.packages("tidyverse")
 install.packages("xgboost")
+
+
 
